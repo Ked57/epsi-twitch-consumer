cca41b1fb4ace8b6c4337da76804afc756fd7d31
Maël <shyndard@gmail.com>
2019-10-08 22:33:52 +0200

    Increased game count and add doc (#2)

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,12 @@ const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID || "";
 const API_URL = process.env.API_URL || "";
 const API_PATH = process.env.API_PATH || "";
 
+// Get top 100 games played on twitch
+// According to the official documentation of twitch, 
+// it is impossible to recover more than 100 occurence.
+// https://dev.twitch.tv/docs/v5/reference/games/
 const main = async () => {
-  const response = await fetch(TWITCH_URL, {
+  const response = await fetch(TWITCH_URL + "?limit=100", {
     method: "GET",
     headers: {
       "Client-ID": TWITCH_CLIENT_ID,
@@ -20,6 +24,7 @@ const main = async () => {
 
   const result = await response.json();
 
+  // Wait for all request on the API are done before continuing
   await Promise.all(
     result.top.map((element: any) => {
       saveGameViewerCount(element.game.name, element.viewers).catch(err =>
@@ -29,8 +34,9 @@ const main = async () => {
   );
 };
 
+// Call an API with game name and viewer count
 const saveGameViewerCount = async (gameName: string, viewerCount: string) => {
-  const response = await fetch(API_URL + API_PATH, {
+  const response = await fetch(API_URL + API_PATH , {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ game: gameName, viewerCount: viewerCount })
@@ -40,12 +46,14 @@ const saveGameViewerCount = async (gameName: string, viewerCount: string) => {
   }
 };
 
+// Get twitch game data each minute
 setInterval(() => {
   main()
     .then(() => console.log("Successful adding twitch rows"))
     .catch(err => console.error(err));
 }, 60 * 1000);
 
+// Run the process at the beginning
 main()
   .then(() => console.log("Successful adding twitch rows"))
   .catch(err => console.error(err));
