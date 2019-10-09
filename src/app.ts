e587da16e5c9de15f636aeb732d15b2a886325e8
unknown <shyndard@gmail.com>
2019-10-09 22:32:37 +0200

    Add rabbitMQ

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
 import fetch from "node-fetch";
+import amqp from 'amqplib/callback_api';
 
 const TWITCH_URL = process.env.TWITCH_URL || "";
 const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID || "";
-const API_URL = process.env.API_URL || "";
-const API_PATH = process.env.API_PATH || "";
-const API_TOKEN = process.env.API_TOKEN || "";
+const MQ_HOST = process.env.MQ_HOST || "";
+const MQ_PORT = process.env.MQ_PORT || "";
+const MQ_USERNAME = process.env.MQ_USERNAME || "";
+const MQ_PASSWORD = process.env.MQ_PASSWORD || "";
+const MQ_CHANNEL = process.env.MQ_CHANNEL || "";
+
+// Init RabbitMQ Channel
+let ch: amqp.Channel;
+amqp.connect(`amqp://${MQ_USERNAME}:${MQ_PASSWORD}@${MQ_HOST}:${MQ_PORT}`, function (err, conn) {
+  conn.createChannel(function (err, channel) {
+      
+      channel.assertQueue(MQ_CHANNEL, {
+        durable: false
+      });
+
+      ch = channel;
+
+      main()
+      .then(() => console.log("Successful adding twitch rows"))
+      .catch(err => console.error(err));
+  });
+});
+
 
 // Get top 100 games played on twitch
 // According to the official documentation of twitch,
@@ -25,39 +46,19 @@ const main = async () => {
 
   const result = await response.json();
 
-  // Wait for all request on the API are done before continuing
-  await Promise.all(
-    result.top.map((element: any) => {
-      saveGameViewerCount(element.game.name, element.viewers).catch(err =>
-        console.error(err)
-      );
-    })
-  );
-};
-
-// Call an API with game name and viewer count
-const saveGameViewerCount = async (gameName: string, viewerCount: string) => {
-  const response = await fetch(`${API_URL}/${API_PATH}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${API_TOKEN}`
-    },
-    body: JSON.stringify({ game: gameName, viewerCount: viewerCount })
+  // Prepare payload 
+  var payLoad = [] as any[];
+  result.top.map((element: any) => {
+    payLoad.push({game: element.game.name, viewerCount: element.viewers});
   });
-  if (!response.ok) {
-    throw response;
-  }
+  
+  // Send message to rabbitMQ
+  ch.sendToQueue(MQ_CHANNEL, Buffer.from(JSON.stringify(payLoad)));
 };
 
 // Get twitch game data each minute
 setInterval(() => {
   main()
-    .then(() => console.log("Successful adding twitch rows"))
+    .then(() => console.log("Successful sending twitch rows"))
     .catch(err => console.error(err));
 }, 60 * 1000);
-
-// Run the process at the beginning
-main()
-  .then(() => console.log("Successful adding twitch rows"))
-  .catch(err => console.error(err));
